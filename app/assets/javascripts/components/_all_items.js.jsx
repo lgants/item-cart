232d0f7cdbea8919ccfb40348a841cdf8ef81c4b
lgants <lgants@gmail.com>
2017-02-09 17:10:54 -0600

    implemented deletion of an item

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,10 +1,16 @@
 var AllItems = React.createClass({
+    handleDelete(id) {
+        this.props.handleDelete(id);
+    },
+
+    // The bind() method binds the id of the item to this, causing the id to be sent as an argument to handleDelete
     render() {
       var items = this.props.items.map((item) => {
           return (
               <div key={item.id}>
                   <h3>{item.name}</h3>
                   <p>{item.description}</p>
+                  <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
               </div>
           )
       });
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -10,11 +10,28 @@ var Body = React.createClass({
         var newState = this.state.items.concat(item);
         this.setState({ items: newState })
     },
+    handleDelete(id) {
+        // changed syntax of the success function from 'success (response) {}' to 'success: () => {}', otherwise it refered to the promise of the response instead of the component itself
+        $.ajax({
+            url: `/api/v1/items/${id}`,
+            type: 'DELETE',
+            success:() => {
+               this.removeItemClient(id);
+            }
+        });
+    },
+    removeItemClient(id) {
+        var newItems = this.state.items.filter((item) => {
+            return item.id != id;
+        });
+
+        this.setState({ items: newItems });
+    },
     render() {
         return (
             <div>
                 <NewItem handleSubmit={this.handleSubmit}/>
-                <AllItems items={this.state.items} />
+                <AllItems items={this.state.items} handleDelete={this.handleDelete}/>
             </div>
         )
     }
