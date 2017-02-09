36455fe0de25289d74441b71ea92aca7a46e94ff
lgants <lgants@gmail.com>
2017-02-09 17:39:06 -0600

    implemented update of an item

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -2,16 +2,18 @@ var AllItems = React.createClass({
     handleDelete(id) {
         this.props.handleDelete(id);
     },
-
+    onUpdate(item) {
+        this.props.onUpdate(item);
+    },
     // The bind() method binds the id of the item to this, causing the id to be sent as an argument to handleDelete
     render() {
       var items = this.props.items.map((item) => {
           return (
-              <div key={item.id}>
-                  <h3>{item.name}</h3>
-                  <p>{item.description}</p>
-                  <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
-              </div>
+            <div key={item.id}>
+                <Item item={item}
+                      handleDelete={this.handleDelete.bind(this, item.id)}
+                      handleUpdate={this.onUpdate}/>
+            </div>
           )
       });
 
