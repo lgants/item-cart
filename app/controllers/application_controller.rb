3ac74fa4c73356a6f2ad40c8b325f56cd41c62bd
lgants <lgants@gmail.com>
2017-02-09 14:34:15 -0600

    added react-rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  # makes controller throw a null session because app will request json, which is different than html, which is default
+  protect_from_forgery with: :null_session
 end
